c9df77be0d3ef3dc20ac42eb479f151bcde1b52f
Raphael <raphaelaugusto@tutanota.com>
2022-08-31 19:19:30 -0300

    Initial Commit

---
+++ AppMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMusic
+//
+//  Created by Raphael Augusto on 31/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
