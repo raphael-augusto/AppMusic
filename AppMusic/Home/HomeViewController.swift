a97a5c174b5c5348cb6c205dea165471889c3427
Raphael <raphaelaugusto@tutanota.com>
2022-09-02 16:14:07 -0300

    feat: add delegate and dataSource UITableView

--- AppMusic/Home/HomeViewController.swift
+++ AppMusic/Home/HomeViewController.swift
@@ -11,13 +11,44 @@ class HomeViewController: UIViewController {
 
     var screen: HomeViewControllerScreen?
     
+    
     override func loadView() {
         self.screen = HomeViewControllerScreen()
+        self.screen?.configureTableViewProtocolos(delegate: self, dataSource: self)
         self.view   = self.screen
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+
+//MARK: - Delegate and DataSource
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CardData.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CardViewTableViewCell.identifier, for: indexPath) as? CardViewTableViewCell
+        cell?.setupCell(data: CardData[indexPath.row])
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    
+    //Redirecting to another screen
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    //Height For cell
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 500
+    }
+}
