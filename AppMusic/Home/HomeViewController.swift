a195044088b1035a272311adddc64c5ebc702605
Raphael <raphaelaugusto@tutanota.com>
2022-09-01 23:45:54 -0300

    feat: add components tableView HomeViewControllerScreen

--- AppMusic/Home/HomeViewController.swift
+++ AppMusic/Home/HomeViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    var screen: HomeViewControllerScreen?
+    
+    override func loadView() {
+        self.screen = HomeViewControllerScreen()
+        self.view   = self.screen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .red
     }
-
-
 }
 
