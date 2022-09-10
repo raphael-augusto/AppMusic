0bf8eb809bc9addc525995cd247e91f02e5acfa1
Raphael <raphaelaugusto@tutanota.com>
2022-09-09 22:55:09 -0300

    feat: add components and constraints card

--- AppMusic/CustomView/CardActionView.swift
+++ AppMusic/CustomView/CardActionView.swift
@@ -66,11 +66,61 @@ class CardActionView: UIView, ConfigurableView  {
         return button
     }()
     
+    private lazy var notInterestedButton: UIButton = {
+        let button                  = UIButton()
+        button.backgroundColor      = .white.withAlphaComponent(0.3)
+        button.layer.cornerRadius   = 22.5
+        button.tintColor            = .white
+        button.clipsToBounds        = true
+        button.setBackgroundImage(UIImage(named: "restrict")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var playButton: UIButton = {
+        let button                  = UIButton()
+        button.backgroundColor      = .white
+        button.layer.cornerRadius   = 35
+        button.tintColor            = .black
+        button.clipsToBounds        = true
+        button.setBackgroundImage(UIImage(named: "playBtn")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button                  = UIButton()
+        button.backgroundColor      = .white.withAlphaComponent(0.3)
+        button.layer.cornerRadius   = 22.5
+        button.tintColor            = .white
+        button.clipsToBounds        = true
+        button.setBackgroundImage(UIImage(named: "like")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var moreButton: UIButton = {
+        let button                  = UIButton()
+        button.backgroundColor      = .white.withAlphaComponent(0.3)
+        button.layer.cornerRadius   = 22.5
+        button.tintColor            = .white
+        button.clipsToBounds        = true
+        button.setBackgroundImage(UIImage(named: "more")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     
     
     //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame )
+        
+        setup()
     }
     
     required init?(coder: NSCoder) {
@@ -89,11 +139,66 @@ class CardActionView: UIView, ConfigurableView  {
 extension CardActionView {
     
     func initSubviews() {
-        <#code#>
+        addSubview(stackView)
+        stackView.addArrangedSubview(notInterestedView)
+        stackView.addArrangedSubview(playView)
+        stackView.addArrangedSubview(likeView)
+        
+        downloadView.addSubview(downloadButton)
+        notInterestedView.addSubview(notInterestedButton)
+        playView.addSubview(playButton)
+        likeView.addSubview(likeButton)
+        moreView.addSubview(moreButton)
     }
     
     
     func initConstraints() {
-        <#code#>
+        stackView.pin(to: self)
+        
+        NSLayoutConstraint.activate([
+            downloadButton.centerXAnchor.constraint(equalTo: downloadView.centerXAnchor),
+            downloadButton.centerYAnchor.constraint(equalTo: downloadView.centerYAnchor),
+            downloadButton.widthAnchor.constraint(equalToConstant: 45),
+            downloadButton.heightAnchor.constraint(equalToConstant: 45),
+            
+            notInterestedButton.centerXAnchor.constraint(equalTo: notInterestedView.centerXAnchor),
+            notInterestedButton.centerYAnchor.constraint(equalTo: notInterestedView.centerYAnchor),
+            notInterestedButton.widthAnchor.constraint(equalToConstant: 45),
+            notInterestedButton.heightAnchor.constraint(equalToConstant: 45),
+        
+            playButton.centerXAnchor.constraint(equalTo: playView.centerXAnchor),
+            playButton.centerYAnchor.constraint(equalTo: playView.centerYAnchor),
+            playButton.widthAnchor.constraint(equalToConstant: 70),
+            playButton.heightAnchor.constraint(equalToConstant: 70),
+            
+            likeButton.centerXAnchor.constraint(equalTo: likeView.centerXAnchor),
+            likeButton.centerYAnchor.constraint(equalTo: likeView.centerYAnchor),
+            likeButton.widthAnchor.constraint(equalToConstant: 45),
+            likeButton.heightAnchor.constraint(equalToConstant: 45),
+            
+            moreButton.centerXAnchor.constraint(equalTo: moreView.centerXAnchor),
+            moreButton.centerYAnchor.constraint(equalTo: moreView.centerYAnchor),
+            moreButton.widthAnchor.constraint(equalToConstant: 45),
+            moreButton.heightAnchor.constraint(equalToConstant: 45),
+        ])
+    }
+    
+    // Layout Card
+    public func updateLayout(for mode: ViewMode) {
+        if mode == .full {
+            downloadView.isHidden = false
+            moreView.isHidden = false
+            stackView.addArrangedSubview(downloadView)
+            stackView.addArrangedSubview(notInterestedView)
+            stackView.addArrangedSubview(playView)
+            stackView.addArrangedSubview(likeView)
+            stackView.addArrangedSubview(moreView)
+            
+        } else {
+            downloadView.isHidden = true
+            moreView.isHidden = true
+            stackView.removeArrangedSubview(downloadView)
+            stackView.removeArrangedSubview(moreView)
+        }
     }
 }
