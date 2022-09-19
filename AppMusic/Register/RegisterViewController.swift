f8f6fd552781fab10ae3f8ab0c7c0c44a43abda2
Raphael <raphaelaugusto@tutanota.com>
2022-09-18 23:47:40 -0300

    feat: add screen Register

--- AppMusic/Register/RegisterViewController.swift
+++ AppMusic/Register/RegisterViewController.swift
@@ -6,24 +6,94 @@
 //
 
 import UIKit
+import Firebase
+
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    var registerScreen: RegisterScreen?
+    
+    var firastore: Firestore?
+    var auth:Auth?
+    
+    var alert: Alert?
+    
+        
+    override func loadView() {
+        self.registerScreen = RegisterScreen()
+        self.view   = self.registerScreen
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.alert  = Alert(controller: self)
+        self.registerScreen?.delegate(delegate: self)
+        self.registerScreen?.configureTextFieldDelegate(delegete: self)
+        
+        self.auth       = Auth.auth()
     }
-    */
+}
 
+
+//MARK: - TextField
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField == self.registerScreen?.emailField {
+            if !textField.validateEmail() {
+                self.alert?.getAlert(title: "Atenção", message: "Digite um e-mail valido.")
+            }
+        }
+
+        
+        if textField == self.registerScreen?.passwordField {
+            if !textField.validatePassword() {
+                self.alert?.getAlert(title: "Senha", message: "Digite no mínimo 6 digitos.")
+            }
+        }
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.registerScreen?.nameField {
+            self.registerScreen?.emailField.becomeFirstResponder()
+        } else if textField == self.registerScreen?.emailField {
+            self.registerScreen?.passwordField.becomeFirstResponder()
+        } else {
+            self.registerScreen?.passwordField.resignFirstResponder()
+        }
+        
+        return true
+    }
+}
+
+
+extension RegisterViewController: RegisterScreenProtocol {
+    func actionRegisterButton() {
+        
+        guard let register = self.registerScreen else { return }
+        
+        //authentication
+        self.auth?.createUser(withEmail: register.getEmail(), password: register.getPassword(), completion: { (result, error) in
+            if error != nil {
+                self.alert?.getAlert(title: "Atenção", message: "Error ao cadastrar")
+            } else {
+                
+                //fireStore - create dataBase (user)
+                if let idUsuario = result?.user.uid {
+                    self.firastore?.collection("usuarios").document(idUsuario).setData([
+                        "nome": self.registerScreen?.getName() ?? "",
+                        "email": self.registerScreen?.getEmail() ?? "",
+                        "id": idUsuario
+                    ])
+                }
+                
+                self.alert?.getAlert(title: "Parabéns", message: "Usuário cadastrado com sucesso.", completion: {
+                    self.dismiss(animated: true, completion: nil)
+                })
+            }
+        })
+    }
 }
--- AppMusic/Register/Screen/RegisterScreen.swift
+++ AppMusic/Register/Screen/RegisterScreen.swift
@@ -7,14 +7,221 @@
 
 import UIKit
 
-class RegisterScreen: UIView {
+protocol RegisterScreenProtocol: AnyObject {
+    func actionRegisterButton()
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class RegisterScreen: UIView, ConfigurableView {
+
+    //MARK: - delegate
+    private weak var delegate: RegisterScreenProtocol?
+    
+    func delegate(delegate: RegisterScreenProtocol?) {
+        self.delegate = delegate
     }
-    */
+    
+    //MARK: - Elements Visual
+    private lazy var titleLabel: UILabel = {
+        let lb            = UILabel()
+        lb.text           = "Cadastro"
+        lb.font           = UIFont.systemFont(ofSize: 48, weight: .medium)
+        lb.textColor      = .white
+        lb.textAlignment  = .left
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        
+        return lb
+    }()
+    
+    lazy var nameField: UITextField = {
+        let tx = UITextField()
+        tx.layer.cornerRadius          = 10
+        tx.clipsToBounds               = true
+        tx.layer.borderWidth           = 2
+        tx.layer.borderColor           = UIColor.systemGray4.cgColor
+        tx.textColor                   = .label
+        tx.tintColor                   = .label
+        tx.textAlignment               = .left
+        tx.font                        = UIFont.preferredFont(forTextStyle: .title2)
+        tx.adjustsFontSizeToFitWidth   = true
+        tx.minimumFontSize             = 12
+        tx.backgroundColor             = .tertiarySystemBackground
+        tx.autocorrectionType          = .no
+        tx.clearButtonMode             = .whileEditing
+        tx.textContentType             = .emailAddress
+        tx.returnKeyType               = .next
+        tx.autocapitalizationType      = .none
+        tx.keyboardType                = .emailAddress
+        tx.attributedPlaceholder       = NSAttributedString(
+                                            string: "Digite seu nome",
+                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.585, green: 0.585, blue: 0.663, alpha: 1)] )
+        tx.translatesAutoresizingMaskIntoConstraints = false
+        
+        tx.setLeftPaddingPoints(10)
+        
+        return tx
+    }()
+    
+    lazy var emailField: UITextField = {
+        let tx = UITextField()
+        tx.layer.cornerRadius          = 10
+        tx.clipsToBounds               = true
+        tx.layer.borderWidth           = 2
+        tx.layer.borderColor           = UIColor.systemGray4.cgColor
+        tx.textColor                   = .label
+        tx.tintColor                   = .label
+        tx.textAlignment               = .left
+        tx.font                        = UIFont.preferredFont(forTextStyle: .title2)
+        tx.adjustsFontSizeToFitWidth   = true
+        tx.minimumFontSize             = 12
+        tx.backgroundColor             = .tertiarySystemBackground
+        tx.autocorrectionType          = .no
+        tx.clearButtonMode             = .whileEditing
+        tx.textContentType             = .emailAddress
+        tx.returnKeyType               = .next
+        tx.autocapitalizationType      = .none
+        tx.keyboardType                = .emailAddress
+        tx.attributedPlaceholder       = NSAttributedString(
+                                            string: "Digite seu email",
+                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.585, green: 0.585, blue: 0.663, alpha: 1)] )
+        tx.translatesAutoresizingMaskIntoConstraints = false
+        
+        tx.setLeftPaddingPoints(10)
+        
+        return tx
+    }()
+    
+    lazy var passwordField: UITextField = {
+        let tx = UITextField()
+        tx.layer.cornerRadius          = 10
+        tx.clipsToBounds               = true
+        tx.layer.borderWidth           = 2
+        tx.layer.borderColor           = UIColor.systemGray4.cgColor
+        tx.textColor                   = .label
+        tx.tintColor                   = .label
+        tx.textAlignment               = .left
+        tx.font                        = UIFont.preferredFont(forTextStyle: .title2)
+        tx.adjustsFontSizeToFitWidth   = true
+        tx.minimumFontSize             = 12
+        tx.backgroundColor             = .tertiarySystemBackground
+        tx.autocorrectionType          = .no
+        tx.clearButtonMode             = .whileEditing
+        tx.textContentType             = .password
+        tx.returnKeyType               = .done
+        tx.autocapitalizationType      = .none
+        tx.isSecureTextEntry           = true
+        tx.keyboardType                = .default
+        tx.attributedPlaceholder       = NSAttributedString(
+                                            string: "Digite sua senha",
+                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.585, green: 0.585, blue: 0.663, alpha: 1)] )
+        tx.translatesAutoresizingMaskIntoConstraints = false
+        
+        tx.setLeftPaddingPoints(10)
+        
+        
+        return tx
+    }()
+    
+    lazy var registerButton: UIButton = {
+        let btn = UIButton()
+        btn.configuration                       = .filled()
+        btn.configuration?.cornerStyle          = .medium
+        btn.configuration?.title                = "Registrar"
+        btn.configuration?.baseBackgroundColor  = UIColor(red: 0.276, green: 0.388, blue: 0.946, alpha: 1)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        
+        btn.addTarget(self, action: #selector(actionRegisterButton), for: .touchUpInside)
+        
+        return btn
+    }()
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Init Components
+    private func setup() {
+        initView()
+        initLayout()
+    }
+    
+    public func configureTextFieldDelegate(delegete: UITextFieldDelegate) {
+        self.nameField.delegate     = delegete
+        self.emailField.delegate    = delegete
+        self.passwordField.delegate = delegete
+    }
+    
+    
+    public func getName() -> String {
+        return self.nameField.text ?? ""
+    }
+    
+    public func getEmail() -> String {
+        return self.emailField.text ?? ""
+    }
+    
+    public func getPassword() -> String {
+        return self.passwordField.text ?? ""
+    }
+}
 
+
+//MARK: - Components and Constraints
+extension RegisterScreen {
+    
+    private func initView() {
+        backgroundColor = UIColor(red: 25/245, green: 25/245, blue: 25/245, alpha: 1)
+    }
+    
+    func initSubviews() {
+        addSubview(titleLabel)
+        addSubview(nameField)
+        addSubview(emailField)
+        addSubview(passwordField)
+        addSubview(registerButton)
+    }
+    
+    
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 60),
+//            titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            nameField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 60),
+            nameField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            nameField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            nameField.heightAnchor.constraint(equalToConstant: 50),
+
+            
+            emailField.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 20),
+            emailField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            emailField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            emailField.heightAnchor.constraint(equalToConstant: 50),
+
+            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 20),
+            passwordField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            passwordField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            passwordField.heightAnchor.constraint(equalToConstant: 50),
+            
+            registerButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 60),
+            registerButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            registerButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            registerButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+}
+
+//MARK: - Action
+extension RegisterScreen: RegisterScreenProtocol {
+    
+    @objc func actionRegisterButton() {
+        self.delegate?.actionRegisterButton()
+    }
 }
